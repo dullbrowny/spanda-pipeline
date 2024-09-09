6e65c2144cf8a043521861101863b2f336e5dc4b
rangaprasad <ranga.rangarajan@spanda.ai>
2024-09-09 21:47:06 +0530

    First version of spanda-pipeline

---
+++ goldenverba/streaming/streaming.py
@@ -0,0 +1,7 @@
+
+def start_streaming_pipeline():
+    print("Streaming pipeline started...")
+
+if __name__ == "__main__":
+    start_streaming_pipeline()
+
---
+++ goldenverba/training/train.py
@@ -0,0 +1,7 @@
+
+def train_model():
+    print("Training model...")
+
+if __name__ == "__main__":
+    train_model()
+
